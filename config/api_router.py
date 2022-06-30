21bab75071313e5834a48660c0bc983cf10663b7
xblzbjs <710255295@qq.com>
2022-06-30 10:59:35 +0800

    feat: add company retrieve and list api

--- config/api_router.py
+++ config/api_router.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
+from jorevi.companies.api.views import CompanyViewSet
 from jorevi.users.api.views import UserViewSet
 
 if settings.DEBUG:
@@ -9,6 +10,6 @@ else:
     router = SimpleRouter()
 
 router.register(r"users", UserViewSet, basename="user")
-
+router.register(r"companies", CompanyViewSet, basename="company")
 
 urlpatterns = router.urls
