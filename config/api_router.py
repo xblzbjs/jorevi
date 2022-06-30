cdafe182cd7f95da36d97c1952cb6d0be2484a9b
xblzbjs <710255295@qq.com>
2022-06-30 17:00:18 +0800

    feat: add apis for jobs app

--- config/api_router.py
+++ config/api_router.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from jorevi.companies.api.views import CompanyViewSet
+from jorevi.jobs.api.views import CategoryViewSet, JobViewSet
 from jorevi.users.api.views import UserViewSet
 
 if settings.DEBUG:
@@ -11,5 +12,7 @@ else:
 
 router.register(r"users", UserViewSet, basename="user")
 router.register(r"companies", CompanyViewSet, basename="company")
+router.register(r"jobs", JobViewSet, basename="job")
+router.register(r"categories", CategoryViewSet, basename="category")
 
 urlpatterns = router.urls
